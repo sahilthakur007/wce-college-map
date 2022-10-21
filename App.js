c780d58f47bbb6fe79b70f456fce074337e11439
sahil thakur <st7378539838@gmail.com>
2022-10-21 19:11:22 +0530

    start

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import MapView from 'react-native-maps';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView style={StyleSheet.absoluteFill}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
+      {/* map */}
     </View>
   );
 }
