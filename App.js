b77fbc3a094d5e1b6fe2337854d3a5712cc44838
sahil thakur <st7378539838@gmail.com>
2022-10-29 21:58:24 +0530

    arcgis

--- App.js
+++ App.js
@@ -1,34 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { useRef } from 'react';
-import MapView from 'react-native-maps';
+import React, { useRef, useEffect } from "react";
+import { loadModules } from "esri-loader";
+
+// import { useEffect, useRef, useState } from "react";
+// import Graphic from '@arcgis/core/Graphic';
+// import { solve } from '@arcgis/core/rest/route';
+// import RouteParameters from '@arcgis/core/rest/support/RouteParameters';
+// import FeatureSet from '@arcgis/core/rest/support/FeatureSet';
+const center = { lat: 18.7939, lng: 73.3346 }
+// const routeUrl = "https://route-api.arcgis.com/arcgis/rest/services/World/Route/NAServer/Route_World";
 export default function App() {
-  const mapRef = useRef();
-  const ne={latitude: 16.8452955,longitude: 74.6046214} 
-  const sw={latitude: 16.8421313,longitude: 74.5993899} 
-  const restrictMap = () => {
-    // console.log("method called")
-    // const nee=mapRef.current.getMapBoundaries();
-    // console.log(nee);
-    // mapRef.current.setMapBoundaries(nee);
-    mapRef.current.setMapBoundaries(northEast=ne,southWest=sw)
-  }
+  
+  const mapEl = useRef(null);
+ 
+  useEffect(
+    () => {
+    
+      let view;
+
+      loadModules(["esri/views/MapView", "esri/WebMap"], {
+        css: true
+      }).then(([MapView, WebMap]) => {
+        
+        const webmap = new WebMap({
+         
+          basemap:"topo-vector",
+          // basemap:"arcgis-nova"
+          
+        });
+
+        // and we show that map in a container
+        view = new MapView({
+          map: webmap,
+          // use the ref as a container
+          zoom:14,
+          container: mapEl.current,
+          center: [73.3346, 18.7939,]
+        });
+      });
+      return () => {
+        // clean up the map view
+        if (!!view) {
+          view.destroy();
+          view = null;
+        }
+      };
+    },
+    // only re-load the map if the id has changed
+    []
+  );
   return (
-    <View style={styles.container}>
-      <MapView 
-        ref={mapRef}
-        style={StyleSheet.absoluteFill}
-        initialRegion={{
-          latitude: 16.8457,
-          longitude: 74.6015,
-          latitudeDelta: 0.009,
-          longitudeDelta: 0.009,
-        }}
-        mapType="satellite"
-        onMapReady={restrictMap}
-      />
-      {/* map */}
-    </View>
+    // <View style={styles.container}  >
+      <div style={{ height: 800 }} ref={mapEl} />
+  //  </View>
   );
 }
 
